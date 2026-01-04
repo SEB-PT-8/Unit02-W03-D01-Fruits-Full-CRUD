68bb90103aa2b061f710b4545ea40f2fc70663ce
omarakamal <omaraakamal@gmail.com>
2026-01-04 19:40:18 +0300

    update

--- server.js
+++ server.js
@@ -10,6 +10,25 @@ app.use(express.static('public')) // my app will serve all static files from pub
 
 
 
+const mongoose = require('mongoose')
+
+async function conntentToDB(){
+    try{
+        // /database_name?
+        await mongoose.connect('mongodb+srv://omar:1234@omar-cluster.dbwrx.mongodb.net/fruitsDB?appName=omar-cluster')
+        console.log('Connection Successful')
+    }
+    catch(err){
+        console.log('Error in Connection')
+    }
+}
+conntentToDB()
+
+
+// Schema and Model
+
+const fruitsSchema = new mongoose.Schema({})
+
 
 // Routes go here
 app.get('/',(req,res)=>{
