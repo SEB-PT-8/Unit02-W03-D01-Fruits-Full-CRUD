3c92f743ba65a4fea0c70be3cd0122244b77a066
omarakamal <omaraakamal@gmail.com>
2026-01-04 19:54:39 +0300

    update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const mongoose = require('mongoose')
 async function conntentToDB(){
     try{
         // /database_name?
-        await mongoose.connect('mongodb+srv://omar:1234@omar-cluster.dbwrx.mongodb.net/fruitsDB?appName=omar-cluster')
+        await mongoose.connect('mongodb+srv://omar:123@omar-cluster.dbwrx.mongodb.net/fruitsDB?appName=omar-cluster')
         console.log('Connection Successful')
     }
     catch(err){
@@ -31,6 +31,16 @@ const fruitsSchema = new mongoose.Schema({
     name:String,
     isReadyToEat: Boolean
 })
+
+const Fruit = mongoose.model('Fruit',fruitsSchema)
+
+
+
+async function createFruit(){
+    const createdFruit = await Fruit.create({name:"Apple",isReadyToEat:true})
+    console.log(createdFruit)
+}
+createFruit()
 
 
 // Routes go here
